d86556d935e0fcb8cf0b8b0888bdd015295e982d
BhavikOstwal <bhavikostwal45@gmail.com>
2024-02-10 12:19:35 +0530

    Initial Commit

---
+++ FoodConnect/FoodConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodConnectApp.swift
+//  FoodConnect
+//
+//  Created by Bhavik Ostwal on 10/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
